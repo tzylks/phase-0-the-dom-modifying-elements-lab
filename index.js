b336edf92c2c8db37388cda000b872f10733cee2
TrevorZylks <trevorzylks@gmail.com>
2021-05-20 16:52:43 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+
+document.body.removeChild(main);
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "YOUR-NAME is the champion";
